ab065214f98c7a65115e17a711c20b03c0213fe9
KristiKo <37247315+KristiKo@users.noreply.github.com>
2018-04-08 20:04:59 +0100

    Add files via upload

---
+++ salaryCalculator.js
@@ -0,0 +1,7 @@
+var myNum = document.getElementById("salaryGross");
+
+document.getElementById("calculate").onclick = function(){
+    
+    document.getElementById("output").innerHTML = myNum.value;
+
+};
